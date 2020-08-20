dd8be9e1cab9c12397238dffa8356d8d5e4faef3
Piyush Kumar <piyush@compgeom.com>
2020-08-19 23:08:19 -0400

    format

--- sr/cutter.py
+++ sr/cutter.py
@@ -12,10 +12,17 @@ from docopt import docopt
 from pathlib import Path
 import numpy as np
 
+
 def computestats(m):
     upper_quartile = np.percentile(m, 90)
     lower_quartile = np.percentile(m, 10)
-    return (np.mean(m), np.std(m), upper_quartile, lower_quartile)
+    return {
+        "mean": np.mean(m),
+        "std": np.std(m),
+        "upper_quartile": upper_quartile,
+        "lower_quartile": lower_quartile,
+    }
+
 
 def process(ifile, ofile):
     print("Processing: ", ifile)
@@ -25,20 +32,25 @@ def process(ifile, ofile):
     prefix = ofile.stem
     odir = ofile.parent
     os.makedirs(odir)
-    #mlist = matrixcutter(m)
+    # mlist = matrixcutter(m)
     # Fill this direcory up with prefix_xx_xx.npz files.
 
+
 def scan_idir(ipath, opath):
-    return [ (x, opath / str(x)[len(str(ipath))+1:]) for x in sorted(ipath.rglob("*.npz"))]
+    return [
+        (x, opath / str(x)[len(str(ipath)) + 1 :]) for x in sorted(ipath.rglob("*.npz"))
+    ]
+
 
 def main():
-    arguments = docopt(__doc__, version='Matrix cutter system')
-    idir = Path(arguments['--input-directory'])
-    odir = Path(arguments['--output-directory'])
+    arguments = docopt(__doc__, version="Matrix cutter system")
+    idir = Path(arguments["--input-directory"])
+    odir = Path(arguments["--output-directory"])
     assert not odir.is_dir(), "Please provide a non-existent output directory!"
     L = scan_idir(idir, odir)
     for inpfile, outfile in L:
         process(inpfile, outfile)
-    
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     main()
