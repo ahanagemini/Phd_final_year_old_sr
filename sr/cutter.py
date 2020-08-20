e9bf5fdbb1eb552efec0d5237a14ded43ed0e9b2
Piyush Kumar <piyush@compgeom.com>
2020-08-19 23:02:50 -0400

    output directory must be empty

--- sr/cutter.py
+++ sr/cutter.py
@@ -7,6 +7,7 @@ cutter.py --input-directory=IDIR --output-directory=ODIR
 
 Example: python3.8 sr/cutter.py --input-directory=idata --output-directory=mdata
 """
+import os
 from docopt import docopt
 from pathlib import Path
 import numpy as np
@@ -21,8 +22,11 @@ def process(ifile, ofile):
     m = np.load(ifile)
     m = m.f.arr_0  # Load data from inside file.
     stats = computestats(m)
+    prefix = ofile.stem
+    odir = ofile.parent
+    os.makedirs(odir)
     #mlist = matrixcutter(m)
-    # Write this out to ofile path
+    # Fill this direcory up with prefix_xx_xx.npz files.
 
 def scan_idir(ipath, opath):
     return [ (x, opath / str(x)[len(str(ipath))+1:]) for x in sorted(ipath.rglob("*.npz"))]
@@ -31,6 +35,7 @@ def main():
     arguments = docopt(__doc__, version='Matrix cutter system')
     idir = Path(arguments['--input-directory'])
     odir = Path(arguments['--output-directory'])
+    assert not odir.is_dir(), "Please provide a non-existent output directory!"
     L = scan_idir(idir, odir)
     for inpfile, outfile in L:
         process(inpfile, outfile)
