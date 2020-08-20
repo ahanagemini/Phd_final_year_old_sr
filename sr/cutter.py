da85369f852b297d3cba5c4d9f60e91880a8b04a
Piyush Kumar <piyush@compgeom.com>
2020-08-19 22:32:16 -0400

    matrix cutting

--- sr/cutter.py
+++ sr/cutter.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python3
+"""Usage: cutter.py 
+cutter.py --input-directory=IDIR --output-directory=ODIR
+
+--input-directory=IDIR  Some directory [default: ./data]
+--output-directory=ODIR  Some directory [default: ./mdata]
+
 """
-"""
+from docopt import docopt
+
+def main():
+    arguments = docopt(__doc__, version='Matrix cutter system')
+    print(arguments)
+
+if __name__ == '__main__':
+    main()
