2345f86627efd958ad2d2e6ea355b11203072d4f
venkat <vb19h@my.fsu.edu>
2020-08-20 21:25:19 -0400

    converting PIL Image to Array is missed after opening the Image

--- sr/cutter.py
+++ sr/cutter.py
@@ -36,7 +36,7 @@ def Loader(ifile):
     ImageArrayPaths = [".npy", ".npz"]
     fileExt = os.path.splitext(ifile.name)[1].lower()
     if fileExt in ImagePaths:
-        image = Image.open(ifile)
+        image = np.array(Image.open(ifile))
     if fileExt == ".npz":
         image = np.load(ifile)
         image = image.f.arr_0  # Load data from inside file.
--- tests/test_sr.py
+++ tests/test_sr.py
@@ -1,5 +1,8 @@
 from sr import __version__
-
+import unittest
+from PIL import Image
+import numpy as np
+from pathlib import Path
 
 def test_version():
     assert __version__ == '0.1.0'
@@ -7,3 +10,12 @@ def test_version():
 from sr import cutter
 
 # TODO: Write a test here for using cutter
+class TestLoader(unittest.TestCase):
+    def test_load(self):
+        path = Path(r"/home/venkat/Documents/PiyushKumarProject/sr/tests/TestFiles/")
+        files = sorted(path.rglob("*.png"))
+        for file in files:
+            self.assertTrue((cutter.Loader(file)).all() == (np.array(Image.open(file))).all())
+
+if __name__ == '__main__':
+        unittest.main()
