62a111c92a71b3a944663157484d41900acebdbb
Piyush Kumar <piyush@compgeom.com>
2020-08-25 19:46:28 -0400

    bugfix

--- sr/testdata.py
+++ sr/testdata.py
@@ -2,6 +2,8 @@
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv[1] != 2):
+    
+    if len(sys.argv) != 2:
         print("Usage: testdata.py data-directory-path")
+        sys.exit(1)
     fpath = sys.argv[1]
