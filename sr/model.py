5cf6a260a7b7ca9a8bc82b59137a40b8adf666b4
Piyush Kumar <piyush@compgeom.com>
2020-09-03 13:35:55 -0400

    updating validation logging

--- sr/model.py
+++ sr/model.py
@@ -107,10 +107,10 @@ def training(training_generator, validation_generator, device, log_dir):
                 loss_valid_list.append(loss_valid.item())
                 vavloss += loss_valid.item()
 
-            # valid log summary after every 10 epochs
-            log_loss_summary(logger, loss_valid_list, step, prefix="val_")
-            loss_valid_list = []
-            del x_valid, y_valid, loss_valid_list
+        # valid log summary after every 10 epochs
+        log_loss_summary(logger, loss_valid_list, step, prefix="val_")
+        loss_valid_list = []
+        del x_valid, y_valid, loss_valid_list
         print("the training loss is {:3.5f} and validation loss is {:3.5f} in epoch {}".format(tavloss / imax, vavloss / imax, epoch))
 
         torch.save(unet.state_dict(), os.getcwd() + "unet_model.pt")
