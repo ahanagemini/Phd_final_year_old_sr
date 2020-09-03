c5f6a971f7ff8d97a4fccd776b1e96bb70da377c
venkat <vb19h@my.fsu.edu>
2020-09-03 15:16:49 -0400

    Test case for ssim

---
+++ sr/test_ssim.py
@@ -0,0 +1,23 @@
+from losses import SSIM
+import numpy as np
+import torch
+
+criterion = SSIM()
+#case 1 same image
+y_pred = []
+for i in range(5):
+    y_pred.append(np.random.rand(1, 256, 256))
+
+y_pred = torch.tensor(y_pred, dtype=torch.float32)
+y_true = y_pred
+loss = criterion(y_pred, y_true)
+print("same image SSIM loss is {}".format(loss))
+
+#case 2 different image
+y_true_diff = []
+for i in range(5):
+    y_true_diff.append(np.random.rand(1, 256, 256))
+
+y_true_diff = torch.tensor(y_true_diff,  dtype=torch.float32)
+loss = criterion(y_pred, y_true_diff)
+print("different image SSIM loss is {}".format(loss))
