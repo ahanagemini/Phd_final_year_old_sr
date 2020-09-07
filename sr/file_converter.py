b86a269d54f26805756299fb99e43db06d03f618
venkat <vb19h@my.fsu.edu>
2020-09-07 16:03:23 -0400

    small changes for file_converter.py to support recursion for file loading

--- sr/file_converter.py
+++ sr/file_converter.py
@@ -15,10 +15,14 @@ input_file = Path(os.getcwd() + r"/DIV2K_train_HR/")
 
 output_file = Path(os.getcwd() +r"/DIV2K_rev/")
 
-for files in os.scandir(input_file):
+extensions = ["*.npz", "*.npy", "*.png", "*.tif", "*.jpeg", "*.jpg", "*.gif"]
+files_list = []
+[files_list.extend(input_file.rglob(x)) for x in extensions]
+
+for i, files in enumerate(files_list):
     file_folder_name = os.path.splitext(files.name)[0]
-    file_name = file_folder_name + ".npy"
+    file_name = file_folder_name
     image = Image.open(files.path)
     image = image.convert(mode="L")
     os.makedirs(output_file / file_folder_name)
-    np.save(output_file/file_folder_name/file_name, np.array(image))
+    np.savez_compressed(output_file/file_folder_name/file_name, np.array(image))
