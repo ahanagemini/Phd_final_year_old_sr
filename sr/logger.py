03d0fc137c265f3c3c2b58ccc2b620f10262034b
Piyush Kumar <piyush@compgeom.com>
2020-09-13 10:51:21 -0400

    emtpy logging directory before start

--- sr/logger.py
+++ sr/logger.py
@@ -1,9 +1,13 @@
 from torch.utils.tensorboard import SummaryWriter
+import shutil
+
 
 class Logger(object):
     def __init__(self, log_dir):
+        shutil.rmtree(log_dir)
         self.writer = SummaryWriter(log_dir=log_dir)
+
     def scalar_summary(self, tag, value, step):
-        '''took the summary logger from https://github.com/mateuszbuda/brain-segmentation-pytorch/blob/master/logger.py'''
+        """took the summary logger from https://github.com/mateuszbuda/brain-segmentation-pytorch/blob/master/logger.py"""
         self.writer.add_scalar(tag=tag, scalar_value=value, global_step=step)
         self.writer.flush()
