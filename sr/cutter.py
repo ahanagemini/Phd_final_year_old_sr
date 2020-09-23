Modified cutter to allow smaller % of data to be processed, corrected stat computation to train set only

sr/cutter.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 
-"""Usage: cutter.py --input-directory=IDIR --output-directory=ODIR
+"""Usage: cutter.py --input-directory=IDIR --output-directory=ODIR --percentage=100
           cutter.py --help | -help | -h
 
 --input-directory=IDIR  Some directory [default: ./data]
 --output-directory=ODIR  Some directory [default: ./mdata]
+--percentage=100 percentage of data to process [default: 100]
 
 cutter expects the input directory of images to be of the following structure.
 Input_directory->patient_folder->patient_image.
 The Output directory will be as follows Output_directory->train/valid/test->
 patient_folder->patient_image and stats.jsonfile
 
-Example: python3.8 sr/cutter.py --input-directory=idata --output-directory=mdata
+Example: python3.8 sr/cutter.py --input-directory=idata --output-directory=mdata --percentage=100
 
 Options:
 --h | -help | --help
@@ -43,13 +44,15 @@ def loader(ifile):
 
     """
 
-    ImagePaths = [".jpg", ".png", ".jpeg", ".gif", ".tif"]
+    ImagePaths = [".jpg", ".png", ".jpeg", ".gif"]
     ImageArrayPaths = [".npy", ".npz"]
+    TiffPaths = [".tiff", ".tif"]
     fileExt = os.path.splitext(ifile.name)[1].lower()
     if fileExt in ImagePaths:
         image = Image.open(ifile)
         image = np.array(image.convert(mode="L"))
-    if fileExt == ".tiff":
+    if fileExt in TiffPaths:
+        # 16 bit tifffiles are not read correctly by Pillow
         image = tifffile.imread(ifile)
     if fileExt == ".npz":
         image = np.load(ifile)
@@ -124,11 +127,12 @@ def matrix_dictionary_update(
     return file_names_map, key_matrix_map, matrix_key_map, key
 
 
-def process(L, stats_path):
+def process(L, stats_paths, train_size):
     """
 
     :param L: contains the input_file path and output_file path
-    :param stats_path: contains the path for the stats json file
+    :param stats_paths: contains the paths for the stats json files
+    :param train_size: train set size
     :return:
     """
     matrices = []
@@ -143,32 +147,35 @@ def process(L, stats_path):
     total_variance = 0.0
     min = 0
     max = 0
+    train_len = int(train_size * len(L)) 
     for i, (ifile, ofile) in enumerate(L):
-        print("processing" + str(ifile), end=" ")
+        # print("processing" + str(ifile), end=" ")
         imatrix = loader(ifile)
         file_name = os.path.splitext(ifile.name)[0]
         if imatrix.shape[0] < 256 or imatrix.shape[1] < 256:
             print("Skipping because file is constant or size is too small.")
             continue
-        matrix_vector = np.asarray(imatrix).reshape(-1)
-        square_vector = np.square(matrix_vector, dtype=np.float)
-        matrix_sum = np.sum(matrix_vector, dtype=np.float)
-        square_sum = np.sum(square_vector, dtype=np.float)
-        matrix_count = len(matrix_vector)
 
-        # this information is for total mean calculation
-        total_sum = total_sum + matrix_sum
-        total_square_sum = total_square_sum + square_sum
-        total_count = total_count + matrix_count
+        matrix_vector = np.asarray(imatrix).reshape(-1)
+        if i < train_len:
+            square_vector = np.square(matrix_vector, dtype=np.float)
+            matrix_sum = np.sum(matrix_vector, dtype=np.float)
+            square_sum = np.sum(square_vector, dtype=np.float)
+            matrix_count = len(matrix_vector)
+
+            # this information is for total mean calculation
+            total_sum = total_sum + matrix_sum
+            total_square_sum = total_square_sum + square_sum
+            total_count = total_count + matrix_count
 
         # maximum and minimum
-        matrix_max = np.max(matrix_vector)
-        matrix_min = np.min(matrix_vector)
-        if max < matrix_max:
-            max = matrix_max
+            matrix_max = np.max(matrix_vector)
+            matrix_min = np.min(matrix_vector)
+            if max < matrix_max:
+                max = matrix_max
 
-        if min > matrix_min:
-            min = matrix_min
+            if min > matrix_min:
+                min = matrix_min
 
         file_names_map, key_matrix_map, matrix_key_map, key = matrix_dictionary_update(
             key_matrix_map,
@@ -179,7 +186,7 @@ def process(L, stats_path):
             ofile,
             file_name,
         )
-        print("\n")
+        # print("\n")
 
     total_mean = total_sum / total_count
     total_variance = (total_square_sum / total_count) - (total_sum / total_count) ** 2
@@ -203,8 +210,9 @@ def process(L, stats_path):
             fname = str(prefix) + "_" + str(i) + "_" + str(j)
             np.savez_compressed(odir / fname, mat)
     # saving stats file in train directory
-    with open(stats_path / "stats.json", "w") as outfile:
-        json.dump(stats, outfile)
+    for i in range(len(stats_paths)):
+        with open(stats_paths[i] / "stats.json", "w") as outfile:
+            json.dump(stats, outfile)
 
     print("Done")
     del (
@@ -223,11 +231,12 @@ def process(L, stats_path):
     )
 
 
-def scan_idir(ipath, opath, train_size=0.9, valid_size=0.05):
+def scan_idir(ipath, opath, percentage, train_size=0.9, valid_size=0.05):
     """
     Returns (x,y) pairs so that x can be processed to create y
+            train length to decide the images over which stats are computed
     """
-    extensions = ["*.npy", "*.npz", "*.png", "*.jpg", "*.gif", "*.tif", "*.jpeg"]
+    extensions = ["*.npy", "*.npz", "*.png", "*.jpg", "*.gif", "*.tif", "*.jpeg", "*.tiff"]
     folders_list = []
     folders_files = []
     folder_file_map = {}
@@ -244,28 +253,32 @@ def scan_idir(ipath, opath, train_size=0.9, valid_size=0.05):
             folders_list.append(folder_name)
             input_folder = Path(input_folder)
             [folders_files.extend(input_folder.rglob(x)) for x in extensions]
+            size = int(len(folders_files) * (percentage / 100))
+            folders_files = folders_files[:size]
             folder_file_map[folder_name] = folders_files
         folders_files = []
     paths = ["train", "test", "valid"]
     folder_input_output_map = {}
-    stats_path = Path("")
     for folder in folders_list:
         L = []
+        stats_paths = []
         folder_files = folder_file_map[folder]
         for i, files in enumerate(folder_files):
             if i < int(train_size * len(folder_files)):
                 L.append((files, opath / paths[0] / folder))
-                stats_path = Path(opath / paths[0] / folder)
+                stats_paths.append(Path(opath / paths[0] / folder))
 
             elif i >= int(train_size * len(folder_files)) and i < int(
                 (train_size + valid_size) * len(folder_files)
             ):
                 L.append((files, opath / paths[1] / folder))
+                stats_paths.append(Path(opath / paths[1] / folder))
             else:
                 L.append((files, opath / paths[2] / folder))
-        folder_input_output_map[folder] = L, stats_path
+                stats_paths.append(Path(opath / paths[2] / folder))
+        folder_input_output_map[folder] = L, stats_paths
 
-    return folder_input_output_map
+    return folder_input_output_map, train_size
 
 
 def main():
@@ -275,12 +288,14 @@ def main():
     arguments = docopt(__doc__, version="Matrix cutter system")
     idir = Path(arguments["--input-directory"])
     odir = Path(arguments["--output-directory"])
+    percentage = int(arguments["--percentage"])
     assert not odir.is_dir(), "Please provide a non-existent output directory!"
-    folder_map = scan_idir(idir, odir)
-
+    folder_map, train_size = scan_idir(idir, odir, percentage)
+    print(folder_map.keys())
     for folder in folder_map.keys():
-        L, stats_path = folder_map[folder]
-        process(L, stats_path)
+        print(folder)
+        L, stats_paths = folder_map[folder]
+        process(L, stats_paths, train_size)
 
 
 if __name__ == "__main__":