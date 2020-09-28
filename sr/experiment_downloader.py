f33f53767537e63b50363803beebd90060758672
Venkat Sumanth Reddy Bommireddy <sumu1526@outlook.com>
2020-09-28 19:03:31 -0400

    created downloader files for downloading input and extracting it to directory

--- sr/experiment_downloader.py
+++ sr/experiment_downloader.py
@@ -10,35 +10,40 @@ Options:
     -h --help -h
 """
 import os
-import sys
 import zipfile
 import tarfile
 from pathlib import Path
 import wget
+from wget import bar_thermometer
 from docopt import docopt
 
-def get_file_name(url, odir):
-    '''
 
-    :param url:
-    :param odir:
-    :return:
-    '''
+def get_file_name(url, odir):
+    """
+
+    :param url: file url
+    :param odir: output directory
+    :return: file_name, file_ext, odir
+    """
     base_name = os.path.basename(url)
-    base_name = str.split(base_name, sep='?')[0]
-    file_name, file_ext = str.split(base_name, sep=".")[0], str.split(base_name, sep=".")[1]
+    base_name = str.split(base_name, sep="?")[0]
+    file_name, file_ext = (
+        str.split(base_name, sep=".")[0],
+        str.split(base_name, sep=".")[1],
+    )
     odir = odir / file_name
 
     return file_name, file_ext, odir
 
-def file_extraction(file_tmp, file_ext, odir):
-    '''
 
-    :param file_tmp:
-    :param file_ext:
-    :param odir:
+def file_extraction(file_tmp, file_ext, odir):
+    """
+
+    :param file_tmp: the downloaded file
+    :param file_ext: the extension of the file
+    :param odir: the output directory of the file
     :return:
-    '''
+    """
     if not os.path.isdir(odir):
         os.makedirs(odir)
     if file_ext == "zip":
@@ -48,23 +53,30 @@ def file_extraction(file_tmp, file_ext, odir):
         tar = tarfile.open(file_tmp)
         tar.extractall(odir)
 
-def bar_custom(current, total, width=80):
-    print("Downloading: %d%% [%d / %d] bytes" % (current / total * 100, current, total))
-    
+
 def data_download(dataname, odir):
-    datadict = {"div2k": r"http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_train_HR.zip",
-                "medical": r"https://www.dropbox.com/s/ri3cpsunqed32my/Medical_data.tar.gz?dl=0"}
-    if dataname=="all":
+    """
+
+    :param dataname: the name of the data to be downloaded or all
+    :param odir: the path where the files will be extracted
+    :return:
+    """
+    datadict = {
+        "div2k": r"http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_train_HR.zip",
+        "medical": r"https://www.dropbox.com/s/ri3cpsunqed32my/Medical_data.tar.gz?dl=0",
+    }
+    if dataname == "all":
         for i, url_key in enumerate(datadict):
             url = datadict[url_key]
             file_name, file_ext, odir = get_file_name(url, odir)
-            file_tmp = wget.download(url, bar=bar_custom)
+            file_tmp = wget.download(url, bar=bar_thermometer)
             file_extraction(file_tmp, file_ext, odir)
     else:
         url = datadict[dataname]
         file_name, file_ext, odir = get_file_name(url, odir)
         file_tmp = wget.download(url)
         file_extraction(file_tmp, file_ext, odir)
+
 
 if __name__ == "__main__":
     arguments = docopt(__doc__)
@@ -73,8 +85,7 @@ if __name__ == "__main__":
     downloader = data_download(down, odir)
 
 
-
-'''
+"""
 print("Enter the download links and press ctrl + d to stop reading\n")
 url_list = sys.stdin.readlines()
 output_directory = Path(os.getcwd() + r"/Image_Input_Directory")
@@ -101,4 +112,4 @@ for url in url_list:
     print("Extracting zip_file")
     with zipfile.ZipFile("download.zip", "r") as Z:
         Z.extractall(out_path, )
-'''
+"""
