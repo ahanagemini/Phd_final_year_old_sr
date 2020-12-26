bd71c9f4e5a92c854236c3d859b7a47ef559fba8
venkat <vb19h@my.fsu.edu>
2020-12-26 07:36:25 -0500

    pushing interpolation code for rgb

--- sr/ssim_calculator.py
+++ sr/ssim_calculator.py
@@ -20,6 +20,48 @@ class SSIM_Calculator:
         image = np.array(image)
         return image
 
+    def writetext_for_stack(self, img_file):
+        img = Image.open(img_file)
+        width, height = img.size
+        draw = ImageDraw.Draw(img)
+        font_path = os.path.abspath(os.path.expanduser('./font/dancing.ttf'))
+        font = ImageFont.truetype(font_path, 15)
+        draw.text((0, 0), "SR", font=font)
+        draw.text((width / 2, 0), "HR", font=font, fill=(0, 0, 255))
+        draw.text((0, width / 2), "Error", font=font, fill=(0, 0, 255))
+        draw.text((height / 2, width / 2), "LR", font=font, fill=(0, 0, 255))
+        img.save(img_file)
+
+
+    def check_dif_matrix(self, hr_image, sr_image):
+        width, height, channels = hr_image.shape
+        diff_matrix_1 = np.zeros((width, height, channels), dtype=np.uint8)
+        for x in range(width):
+            for y in range(height):
+                for z in range(channels):
+                    diff_matrix_1[x][y][z] = np.abs(hr_image[x][y][z] - sr_image[x][y][z])
+
+        diff_matrix_2 = np.abs(hr_image - sr_image)
+
+        for x in range(width):
+            for y in range(height):
+                for z in range(channels):
+                    assert diff_matrix_1[x][y][z] == diff_matrix_2[x][y][z]
+    
+    def error_image_plot(self, hr_image, sr_image, odir):
+        diff_image = np.abs(hr_image - sr_image)
+        diff_image = Image.fromarray(diff_image)
+        lr_image = Image.fromarray(hr_image)
+        lr_image = lr_image.resize((64, 64), resample=Image.BICUBIC)
+        lr_image = lr_image.resize((256, 256), resample=Image.BICUBIC)
+
+        horizontal_stack_1 = np.hstack([sr_image, hr_image])
+        horizontal_stack_2 = np.hstack([diff_image, lr_image])
+        stacked_error = np.vstack([horizontal_stack_1, horizontal_stack_2])
+        odir = odir + r"_stacked.png"
+        plt.imsave(odir, stacked_error, vmax=np.max(sr_image), vmin=np.min(sr_image))
+        self.writetext_for_stack(odir)
+
     def plotter(self, hr_image, sr_image, odir):
         hr_plot_image = np.ravel(hr_image)
         sr_plot_image = np.ravel(sr_image)
@@ -37,6 +79,7 @@ class SSIM_Calculator:
 
         diff_image = np.abs(hr_plot_image - sr_plot_image)
         diff_x = [x for x in range(len(diff_image))]
+        diff_image = sorted(diff_image)
         import matplotlib as mpl
         mpl.rcParams['agg.path.chunksize'] = 10000
         fig = plt.figure(figsize=(20, 20))
@@ -61,6 +104,7 @@ class SSIM_Calculator:
         if not os.path.isdir(odir):
             os.makedirs(odir)
         image_paths = hr_dir.rglob("*.png")
+        top_right_pixel = {}
         average_losses = {}
         ssim_list = []
         l1_loss_list = []
@@ -78,12 +122,17 @@ class SSIM_Calculator:
                     hr_image, sr_image, data_range=vmax
                 )
                 ssim_loss = metrics.structural_similarity(
-                    hr_image, sr_image, data_range=vmax,multichannel=True
+                    hr_image, sr_image, data_range=vmax, multichannel=True
                 )
             out_sr = odir / image_name
+            self.check_dif_matrix(hr_image, sr_image)
+            diff_image = np.abs(np.subtract(hr_image, sr_image))
             self.writetext(sr_image, out_sr, l1_loss, psnr_loss, ssim_loss)
-            self.plotter(hr_image, sr_image, str(odir/(os.path.splitext(image_name)[0])))
-
+            #self.plotter(hr_image, sr_image, str(odir/(os.path.splitext(image_name)[0])))
+            self.error_image_plot(hr_image, sr_image, str(odir/(os.path.splitext(image_name)[0])))
+            top_right_pixel[image_name+"_sr"] = sr_image[0][-1][:].tolist()
+            top_right_pixel[image_name+"_err"] = diff_image[0][-1][:].tolist()
+            top_right_pixel[image_name+"_hr"] = hr_image[0][-1][:].tolist()
             ssim_list.append(ssim_loss)
             psnr_loss_list.append(psnr_loss)
             l1_loss_list.append(l1_loss)
@@ -94,6 +143,8 @@ class SSIM_Calculator:
 
         with open(str(odir/"average_losses.json"), "w") as sfile:
             json.dump(average_losses, sfile)
+        with open(str(odir/ "top_right_pixel.json"), "w") as sfile:
+            json.dump(top_right_pixel, sfile)
 
 class Configurator:
     """this class is for configurating input and output"""
