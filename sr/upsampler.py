dcb8c3befd299c82c6ef9d6c4b480850ec794368
Piyush Kumar <piyush@compgeom.com>
2021-01-12 22:54:57 -0500

    cleaning up required for upsampler.py - lot of unused things here

--- sr/upsampler.py
+++ sr/upsampler.py
@@ -1,23 +1,24 @@
-import os
-import torch
-from torch.backends import cudnn
-
-from test_dataset import Upsampler_Dataset
-from train_util import model_selection, chop_forward, forward_chop
+"""
+This module will be used to upsample input images.
+"""
 import argparse
 import os
-import torch
-from torch.utils.data import Dataset
 from pathlib import Path
-import matplotlib.pyplot as plt
-from torchvision.transforms import Compose
-import scipy.ndimage
-import numpy as np
-from stat_plotter import PlotStat
-from cutter import loader
-from tqdm import tqdm
-from GPUtil import showUtilization as gpu_usage
 
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.ndimage
+import torch
+from GPUtil import showUtilization as gpu_usage
+from torch.backends import cudnn
+from torch.utils.data import Dataset
+from torchvision.transforms import Compose
+from tqdm import tqdm
+
+from cutter import loader
+from stat_plotter import PlotStat
+from test_dataset import Upsampler_Dataset
+from train_util import chop_forward, forward_chop, model_selection
 
 
 def prepare(tensor, conf):
@@ -32,8 +33,10 @@ def prepare(tensor, conf):
     -------
 
     """
-    if conf.precision == 'half': tensor = tensor.half()
+    if conf.precision == "half":
+        tensor = tensor.half()
     return tensor
+
 
 def create_dataset(path, lognorm=False):
     """
@@ -52,6 +55,9 @@ def create_dataset(path, lognorm=False):
 
 
 def upsampler(conf):
+    """
+    The main function that reads configuration and upsamples a directory
+    """
     parameters = {"batch_size": 1, "shuffle": False, "num_workers": 6}
     use_cuda = torch.cuda.is_available()
     device = torch.device("cuda:0" if use_cuda else "cpu")
@@ -64,7 +70,7 @@ def upsampler(conf):
     model = model_selection(conf.architecture, conf.aspp, conf.dilation, conf.act)
     model = model.to(device)
     checkpoint = torch.load(conf.model)
-    model.load_state_dict(checkpoint['model'])
+    model.load_state_dict(checkpoint["model"])
     model.eval()
     if conf.precision == "half":
         model.half()
@@ -78,9 +84,15 @@ def upsampler(conf):
             filename = str(i) + ".png"
             filepath = Path(conf.output) / filename
             sample["lr"] = prepare(sample["lr"], conf)
-            y_pred = forward_chop(sample["lr"].to(device), model=model, scale=4, shave=10)
+
+            # The actual upsample happens here
+            y_pred = forward_chop(
+                sample["lr"].to(device), model=model, scale=4, shave=10
+            )
             y_pred = (y_pred * std) + mean
-            y_pred = np.clip(y_pred.cpu(), stats["min"].cpu().numpy(), stats["max"].cpu().numpy())
+            y_pred = np.clip(
+                y_pred.cpu(), stats["min"].cpu().numpy(), stats["max"].cpu().numpy()
+            )
             if conf.lognorm:
                 image_sign = np.sign(y_pred)
                 y_pred = image_sign * (np.exp(np.abs(y_pred)) - 1.0)
@@ -89,46 +101,67 @@ def upsampler(conf):
             y_pred = y_pred.numpy()
 
             if sample["type"][0] == "height":
-                y_pred = y_pred[sample["pad_1"]*4:-sample["pad_2"]*4, :]
+                y_pred = y_pred[sample["pad_1"] * 4 : -sample["pad_2"] * 4, :]
             elif sample["type"][0] == "width":
-                y_pred = y_pred[:, sample["pad_1"]*4:-sample["pad_2"]*4]
+                y_pred = y_pred[:, sample["pad_1"] * 4 : -sample["pad_2"] * 4]
 
             print(y_pred.shape)
             vmax = np.max(y_pred)
             vmin = np.min(y_pred)
             plt.imsave(str(filepath), y_pred, cmap="gray", vmax=vmax, vmin=vmin)
-            
+
             del y_pred, vmax, vmin, filepath, stats, filename
-    
+
 
 class Configurator:
     """ This is the config class for tester"""
+
     def __init__(self):
         self.parser = argparse.ArgumentParser()
-        self.parser.add_argument('--input', default=os.path.dirname(os.path.abspath(__file__)) +r"/input_dir",
-                                 help="use this command to set input directory")
-        self.parser.add_argument('--output', default=os.path.dirname(os.path.abspath(__file__)) + r"/output_dir",
-                                 help="use this command to set output directory")
-        self.parser.add_argument('--model', default=os.path.dirname(os.path.abspath(__file__)) +r"/model_dir",
-                                 help="use this command to set model directory")
-        self.parser.add_argument('--architecture', default="edsr_16_64",
-                                 help="use this command to set architecture")
-        self.parser.add_argument('--act', default="leakyrelu",
-                                 help="use this command to set activation")
-        self.parser.add_argument('--aspp', default=False,
-                                 help="use this to set aspp for edsr")
-        self.parser.add_argument('--dilation', default=False,
-                                 help="use this to set dilation for edsr")
-        self.parser.add_argument('--lognorm', default=False,
-                                 help="use this command to set lognorm")
-        self.parser.add_argument('--precision', default='half',
-                                 help="use this to set the command to change the precision")
-    
+        self.parser.add_argument(
+            "--input",
+            default=os.path.dirname(os.path.abspath(__file__)) + r"/input_dir",
+            help="use this command to set input directory",
+        )
+        self.parser.add_argument(
+            "--output",
+            default=os.path.dirname(os.path.abspath(__file__)) + r"/output_dir",
+            help="use this command to set output directory",
+        )
+        self.parser.add_argument(
+            "--model",
+            default=os.path.dirname(os.path.abspath(__file__)) + r"/model_dir",
+            help="use this command to set model directory",
+        )
+        self.parser.add_argument(
+            "--architecture",
+            default="edsr_16_64",
+            help="use this command to set architecture",
+        )
+        self.parser.add_argument(
+            "--act", default="leakyrelu", help="use this command to set activation"
+        )
+        self.parser.add_argument(
+            "--aspp", default=False, help="use this to set aspp for edsr"
+        )
+        self.parser.add_argument(
+            "--dilation", default=False, help="use this to set dilation for edsr"
+        )
+        self.parser.add_argument(
+            "--lognorm", default=False, help="use this command to set lognorm"
+        )
+        self.parser.add_argument(
+            "--precision",
+            default="half",
+            help="use this to set the command to change the precision",
+        )
+
     def parse(self, args=None):
         """Parse the configuration"""
         self.conf = self.parser.parse_args(args=args)
         return self.conf
-        
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     conf = Configurator().parse()
     upsampler(conf)
